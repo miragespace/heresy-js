95d55acefddc054520e7f5ed0d538c0e656763dd
Rachel Chen <rachel@chens.email>
2023-03-11 00:52:33 -0800

    react-native-fetch: use raw object instead of Map for easier inter-op with runtime

--- src/react-native-fetch/Headers.ts
+++ src/react-native-fetch/Headers.ts
@@ -23,13 +23,28 @@ function normalizeValue(value: string | number) {
   return value;
 }
 
+function iteratorFor<R = any>(items: R[]): IterableIterator<R> {
+  var dummyR: R;
+  var iterator = {
+    next(): IteratorResult<R> {
+      var value = items.shift();
+      return { done: value === undefined, value: value ?? dummyR };
+    },
+    [Symbol.iterator]() {
+      return iterator;
+    },
+  };
+
+  return iterator;
+}
+
 export type HeadersInit =
   | Headers
   | Record<string, string>
   | [key: string, value: string][];
 
 export default class Headers {
-  map: Map<string, string> = new Map();
+  map: Record<string, string> = {};
 
   constructor(init?: HeadersInit) {
     if (init instanceof Headers) {
@@ -58,48 +73,48 @@ export default class Headers {
   append(name: string, value: string): void {
     name = normalizeName(name);
     value = normalizeValue(value);
-    const oldValue = this.get(name);
+    const oldValue = this.map[name];
     // From MDN: If the specified header already exists and accepts multiple values, append() will append the new value to the end of the value set.
     // However, we're a missing a check on whether the header does indeed accept multiple values
-    this.map.set(name, oldValue ? oldValue + ", " + value : value);
-  }
-
-  delete(name: string): void {
-    this.map.delete(normalizeName(name));
+    this.map[name] = oldValue ? oldValue + ", " + value : value;
   }
 
   get(name: string): string | null {
     name = normalizeName(name);
-    return this.map.get(name) ?? null;
+    return this.map[name] ?? null;
   }
 
   has(name: string): boolean {
-    return this.map.has(normalizeName(name));
+    return this.map.hasOwnProperty(normalizeName(name));
   }
 
   set(name: string, value: string | number): void {
-    this.map.set(normalizeName(name), normalizeValue(value));
+    this.map[normalizeName(name)] = normalizeValue(value);
   }
 
   forEach(
     callback: (value: string, key: string, parent?: object) => void,
     thisArg?: object
   ): void {
-    this.map.forEach((value, name) => {
-      callback.call(thisArg, value, name, this);
-    }, this);
+    for (const key in this.map) {
+      callback.call(thisArg, this.map[key], key, this);
+    }
   }
 
   keys(): IterableIterator<string> {
-    return this.map.keys();
+    return iteratorFor(Object.keys(this.map));
   }
 
   values(): IterableIterator<string> {
-    return this.map.values();
+    return iteratorFor(Object.values(this.map));
   }
 
   entries(): IterableIterator<[string, string]> {
-    return this.map.entries();
+    var items: [string, string][] = [];
+    this.forEach((value, name) => {
+      items.push([name, value]);
+    });
+    return iteratorFor(items);
   }
 
   [Symbol.iterator](): IterableIterator<[string, string]> {
